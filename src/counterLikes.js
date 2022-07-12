210a0b7506b9c7fdf16506fc5e530263b6b5cd2e
Anna <tumasheva.anna.vl@gmail.com>
2022-07-12 22:44:19 +1000

    added a button class btn-like + on, to change button state

--- src/counterLikes.js
+++ src/counterLikes.js
@@ -1,19 +1,41 @@
 import React, { useState } from 'react'
 
+function SvgLike() {
+    return (
+        <svg className='svg-like' viewBox="0 0 32 32" version="1.1" aria-hidden="false" fill="#767676">
+            <path d="M17.4 29c-.8.8-2 .8-2.8 0l-12.3-12.8c-3.1-3.1-3.1-8.2 0-11.4 3.1-3.1 8.2-3.1 11.3 0l2.4 2.8 2.3-2.8c3.1-3.1 8.2-3.1 11.3 0 3.1 3.1 3.1 8.2 0 11.4l-12.2 12.8z"></path>
+        </svg>
+    )
+}
 
- function CounterLikes(props) {
-    const [likes, setCountLikes] = useState(props.likes)
+function BtnLike(props) {
+    return (
+        <button onClick={props.onClick} className={props.className} title="Like">
+            <SvgLike />
+        </button>
+    )
+}
+
+function CounterLikes(props) {
+    const [likesCount, setLikesCount] = useState(props.likes)
+    const [isLiked, setIsLiked] = useState(false)
+
+    let btnClass = 'btn-like'
+    if (isLiked) {
+        btnClass = `${btnClass} on`
+    }
+    
     return (
         <div className="mosaic-infoTop mosaic-text">
-            <button className="btn-like" title="Like" onClick={() => {
-               setCountLikes(likes + 1) 
-            }}>
-                <svg className='svg-like' viewBox="0 0 32 32" version="1.1" aria-hidden="false" fill="#767676">
-                    <path d="M17.4 29c-.8.8-2 .8-2.8 0l-12.3-12.8c-3.1-3.1-3.1-8.2 0-11.4 3.1-3.1 8.2-3.1 11.3 0l2.4 2.8 2.3-2.8c3.1-3.1 8.2-3.1 11.3 0 3.1 3.1 3.1 8.2 0 11.4l-12.2 12.8z"></path>
-                </svg>
-            </button>
-            <div className='mosaic-likes'>💔 {likes}</div>
-        </div> 
+            <BtnLike
+                className={btnClass}
+                onClick={() => {
+                    setIsLiked(!isLiked)
+                    setLikesCount(isLiked ? likesCount - 1 : likesCount + 1)
+                }}
+            />
+            <div className='mosaic-likes'>💔 {likesCount}</div>
+        </div>
     )
 }
 
--- src/nav.js
+++ src/nav.js
@@ -1,4 +1,24 @@
 import LogoSvg from './logo.jsx'
+// import { slide as Menu } from 'react-burger-menu'
+// import React, { useState } from 'react'
+
+// class Example extends React.Component {
+//   showSettings (event) {
+//     event.preventDefault();
+//   }
+
+//   render () {
+//     // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
+//     return (
+//       <Menu>
+//         <a id="home" className="menu-item" href="/">Home</a>
+//         <a id="about" className="menu-item" href="/about">About</a>
+//         <a id="contact" className="menu-item" href="/contact">Contact</a>
+//         <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+//       </Menu>
+//     );
+//   }
+// }
 
 function Navigation(props) {
   return (
@@ -9,6 +29,7 @@ function Navigation(props) {
         <p className="siteName">
         {props.siteName}
         </p>
+        {/* <Example /> */}
       </nav>
     </header>
   )
